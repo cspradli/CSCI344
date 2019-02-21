9094850f3076964d64ce8afdfeee02044c1e4606
Caleb Spradlin <cspradli@unca.edu>
2019-02-20 22:30:42 -0500

    made working version

--- projecttwo/script/main.js
+++ projecttwo/script/main.js
@@ -1,59 +1,85 @@
 const classNames = {
-    TODO_ITEM: 'todo-container',
-    TODO_CHECKBOX: 'todo-checkbox',
-    TODO_TEXT: 'todo-text',
-    TODO_DELETE: 'todo-delete',
-}
+    todoItem: 'todo-container',
+    todoCheck: 'todo-checkbox',
+    todoText: 'todo-text',
+  }
   
   const list = document.getElementById('todo-list')
-  const itemCountSpan = document.getElementById('item-count')
-  const uncheckedCountSpan = document.getElementById('unchecked-count')
-  //Counts
-
+  const itemCountDiv = document.getElementById('item-count')
+  const uncheckedCountDiv = document.getElementById('unchecked-count')
+  
   let todos = []
-function count(){
-    
-}
-function Todo(name){
-    this.name = name || 'new todo'
-    this.checked = false
+
+  function checkCount(){
+    var y = todos.length
+    for(i = 0; i < todos.length; i++){
+        if(todos[i].checked === true){
+            y--
+        }
+    }
+    return y
+  }
+
+  function Todo(name) {
+    this.name = name || 'New TODO'
     this.element = null
+    this.checked = false
     this.checkbox = null
-}
+  }
+  
+  Todo.prototype.check = function() {
+    this.checked = !this.checked
+    if (this.element && this.checkbox) {
+      this.checkbox.checked = this.checked
+    } else {
+      this.element = makeTodo(this)
+    }
+  }
+  
+  function makeTodo(todo) {
+    if (todo.element) return todo.element
+  
+    const checkbox = document.createElement('input')
+    checkbox.className = classNames.todoCheck
+    checkbox.type = 'checkbox'
+    checkbox.checked = todo.checked
 
-function makeTodo(todo){
-  const checkbox = document.createElement('input')
-  checkbox.className = classNames.TODO_CHECKBOX
-  checkbox.type = 'checkbox'
-  checkbox.checked = todo.checked
-
-  const span = document.createElement('span')
-  span.className = classNames.TODO_TEXT
-  span.setAttribute('contenteditable', 'true')
-  span.innerHTML = todo.name
-
-  const li = document.createElement('li')
-  li.className = classNames.TODO_ITEM
-  li.appendChild(checkbox)
-  li.appendChild(span)
-
-  todo.element = li
-  todo.checkbox = checkbox
-
-  return li
-}
-
-function make(){
+    checkbox.todoRef = todo
+    checkbox.onchange = check
+  
+    const span = document.createElement('span')
+    span.className = classNames.todoText
+    span.innerHTML = todo.name
+  
+    const li = document.createElement('li')
+    li.className = classNames.todoItem
+    li.appendChild(checkbox)
+    li.appendChild(span)
+  
+    todo.element = li
+    todo.checkbox = checkbox
+  
+    return li
+  }
+  
+  function make() {
     list.innerHTML = ''
     todos.map(makeTodo).forEach(todo => list.appendChild(todo))
-    uncheckedCountSpan.innerHTML = todos.length
-    itemCountSpan.innerHTML = todos.length
+    uncheckedCountDiv.innerHTML = checkCount()
+    itemCountDiv.innerHTML = todos.length
     return false
-}
-
-function addTodo(name){
+  }
+  
+  function addTodo() {
+    var name = document.getElementById("inputTodo").value
     const todo = new Todo(name)
+    //clears input box
+    document.getElementById("inputTodo").value = ''
     todos.push(todo)
     return make()
-}
-
+  }
+  
+  function check() {
+    this.todoRef.check()
+    return make()
+  }  
